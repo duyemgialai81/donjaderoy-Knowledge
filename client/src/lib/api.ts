c38598dec632d6947182cce887600513926f67a9
duyemgialai81 <doanngocduy62@gmail.com>
2026-05-16 12:25:45 +0700

    duyem

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080';
+const API_BASE = 'https://donjaderoy81-knowledge.hf.space';
 import { localStorage_service } from './localStorage';
 async function safeJson(res: Response) {
   try {
